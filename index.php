a82bdcfd86838c15f31dfadffa97a45bf962ce3f
Audun Myhra Bergwitz <32357781+audunmb@users.noreply.github.com>
2023-05-04 14:30:05 +0200

    Change to javascript registration of styles

--- index.php
+++ index.php
@@ -2,39 +2,27 @@
 
 /**
  * Plugin Name: Gutenberg Block Styles
- * Plugin URI: https://github.com/Automattic/gutenberg-block-styles/
+ * Plugin URI: https://github.com/audunmb/gutenberg-block-styles
  * Description: A simple plugin to demonstrate how to add block styles to Gutenberg.
- * Version: 1.1
- * Author: Automattic
+ * Version: 1.2
+ * Author: audunmb
  */
 
-/**
- * Register Custom Block Styles
- */
-if ( function_exists( 'register_block_style' ) ) {
-	function block_styles_register_block_styles() {
-		/**
-		 * Register stylesheet
-		 */
-		wp_register_style(
-			'block-styles-stylesheet',
-			plugins_url( 'style.css', __FILE__ ),
-			array(),
-			'1.1'
-		);
-
-		/**
-		 * Register block style
-		 */
-		register_block_style(
-			'core/paragraph',
-			array(
-				'name'         => 'blue-paragraph',
-				'label'        => 'Blue Paragraph',
-				'style_handle' => 'block-styles-stylesheet',
-			)
-		);
-	}
-
-	add_action( 'init', 'block_styles_register_block_styles' );
+/* Register the javascript file that for the block styles */
+ function customblockstyles_editor_assets() {
+	wp_enqueue_script(	'custom-block-styles-script', plugins_url( 'custom-block-styles.js', __FILE__  ),
+		  array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+	);
 }
+add_action( 'enqueue_block_editor_assets', 'customblockstyles_editor_assets' );
+
+/* Register CSS for the block styles */
+
+function custom_block_styles() {
+	wp_enqueue_style(
+		'custom-block-styles', plugins_url( 'custom-block-styles.css', __FILE__ ) );
+}
+add_action( 'enqueue_block_assets', 'custom_block_styles' );
+
+	
+
